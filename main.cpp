93236b493acb190d2ce3dd91ab2905e55a0e3451
Kirill Polushin <kirill@polushin.org>
2017-09-04 22:00:21 +0700

    Initial commit.

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TritDataset
+//
+//  Created by Кирилл on 04.09.17.
+//  Copyright © 2017 Кирилл. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ unit_tests/trit_unit_tests.cpp
@@ -0,0 +1,9 @@
+//
+//  trit-unittest.cpp
+//  TritDataset
+//
+//  Created by Кирилл on 04.09.17.
+//  Copyright © 2017 Кирилл. All rights reserved.
+//
+
+#include <stdio.h>
